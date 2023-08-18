d68f6c3a1367d62edc40182e0cdff2303a1cdd55
LeinardB92 <boanergespife@gmail.com>
2023-08-18 17:08:38 -0600

    se crea el serializador para los los 3 modelos que se crearon anteriormente

---
+++ stationApp/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from stationApp.models import Sensors, Actuators, Crops
+
+class SensorsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Sensors
+        fields = '__all__'
+
+class ActuatorsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Actuators
+        fields = '__all__'
+
+class CropsSerializers(serializers.ModelSerializer):
+    model = Crops
+    fields = '__all__'
+
+    
+
+
+
+    
