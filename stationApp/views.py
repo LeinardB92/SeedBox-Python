c9b15d5b23c92b03ae50064387d3d7ad3ef48e25
LeinardB92 <boanergespife@gmail.com>
2023-08-18 17:22:29 -0600

    Se crea el primer intento de  viewset

--- stationApp/views.py
+++ stationApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from stationApp.models import *
+from stationApp.serializers import *
+from rest_framework import viewsets
+from rest_framework import filters
 
 # Create your views here.
+
+class SensorsViewSet(viewsets.ModelViewSet):
+    queryset = Sensors.objects.all()
+    serializer_class = SensorsSerializers
+
+class ActuatorsViewSet(viewsets.ModelViewSet):
+    queryset = Actuators.objects.all()
+    serializer_class = ActuatorsSerializers
+
+class CropsViewSet(viewsets.ModelViewSet):
+    queryset = Crops.objects.all()
+    serializer_class = CropsSerializers
+
+    
+
